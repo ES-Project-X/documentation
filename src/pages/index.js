1c1fa5eba7a63a80c159f5708fa2fd77783acb64
João Fonseca <joao.fonseca@ua.pt>
2023-11-18 17:34:24 +0000

    fix link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/category/user-stories">
             Pedal to the Documentation
           </Link>
         </div>
