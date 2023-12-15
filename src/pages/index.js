9e1709d4c32bef5b6d3ba8e82be15f88096d6488
João Fonseca <joao.fonseca@ua.pt>
2023-12-15 22:28:01 +0000

    fix style for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,44 +8,50 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg margin-horiz--md"
-            to="/docs/category/user-stories">
-            Documentation
-          </Link>
-          <Link
-            className="button button--secondary button--lg margin-horiz--md"
-            href="https://web.project-x.pt">
-            Web App / PWA
-          </Link>
-          <Link
-            className="button button--secondary button--lg margin-horiz--md"
-            href=""> {/* TODO: add link to download page */}
-            Mobile App
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className="row">
+                    <div className={clsx('col col--4')} style={{marginBottom: "2rem"}}>
+                        <Link
+                            className="button button--secondary button--lg margin-horiz--md"
+                            to="/docs/category/user-stories">
+                            Documentation
+                        </Link>
+                    </div>
+                    <div className={clsx('col col--4')} style={{marginBottom: "2rem"}}>
+                        <Link
+                            className="button button--secondary button--lg margin-horiz--md"
+                            href="https://web.project-x.pt">
+                            Web App / PWA
+                        </Link>
+                    </div>
+                    <div className={clsx('col col--4')} style={{marginBottom: "2rem"}}>
+                        <Link
+                            className="button button--secondary button--lg margin-horiz--md"
+                            href=""> {/* TODO: add link to download page */}
+                            Mobile App
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header >
+    );
 }
 
 export default function Home() {
-  // const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title="Home"
-      description="Project X - Community collaboration platform for bicycle users">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    // const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title="Home"
+            description="Project X - Community collaboration platform for bicycle users">
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
 }
